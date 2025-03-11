40d88e22fa9cad9c36a81e33fb4ce02dcc594fb3
Claes Nilsson <claes.nilsson13@gmail.com>
2025-03-11 14:43:56 +0100

    update oled

--- Arduino/VolumeControl/volume_oled.ino
+++ Arduino/VolumeControl/volume_oled.ino
@@ -11,7 +11,7 @@ Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 #define DT 3        // Rotary Encoder Data (B)
 #define SW 4        // Rotary Encoder Button (Mute)
 #define DS1802_CLK 6  // DS1802 Clock
-#define DS1802_UD 7   // DS1802 Up/Down
+#define DS1802_D 7   // DS1802 Data (D)
 #define DS1802_MUTE 5 // DS1802 Mute
 
 bool muteState = false;
@@ -25,14 +25,14 @@ void setup() {
     pinMode(DT, INPUT_PULLUP);
     pinMode(SW, INPUT_PULLUP);
     pinMode(DS1802_CLK, OUTPUT);
-    pinMode(DS1802_UD, OUTPUT);
+    pinMode(DS1802_D, OUTPUT);
     pinMode(DS1802_MUTE, OUTPUT);
 
     digitalWrite(DS1802_MUTE, HIGH);  // Start unmuted
     lastEncoderState = digitalRead(CLK);
 
     // Initialize OLED Display
-    if (!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) {  // 0x3C is the typical I2C address
+    if (!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) {
         while (true);  // Halt if OLED fails
     }
     display.clearDisplay();
@@ -80,10 +80,11 @@ void loop() {
 
 // Function to update DS1802 volume levels
 void updateVolume() {
-    int logVolume = pow(10, (volume / 63.0) * log10(63)); // Logarithmic mapping
+    int leftVol = volume + (balance < 0 ? balance : 0);
+    int rightVol = volume - (balance > 0 ? balance : 0);
 
-    int leftVol = logVolume + (balance < 0 ? balance : 0);
-    int rightVol = logVolume - (balance > 0 ? balance : 0);
+    leftVol = constrain(leftVol, 0, 63);
+    rightVol = constrain(rightVol, 0, 63);
 
     sendToDS1802(leftVol);
     sendToDS1802(rightVol);
@@ -91,9 +92,28 @@ void updateVolume() {
 
 // Function to send volume to DS1802
 void sendToDS1802(int vol) {
-    for (int i = 0; i < 7; i++) {
+    for (int i = 6; i >= 0; i--) {  // Send MSB first
         digitalWrite(DS1802_CLK, LOW);
-        digitalWrite(DS1802_UD, (vol & (1 << i)) ? HIGH : LOW);
+        digitalWrite(DS1802_D, (vol & (1 << i)) ? HIGH : LOW);
         digitalWrite(DS1802_CLK, HIGH);
     }
+}
+
+// Function to update OLED display
+void updateOLED() {
+    display.clearDisplay();
+    display.setTextSize(1);
+    display.setTextColor(SSD1306_WHITE);
+    display.setCursor(0, 0);
+    display.print("Vol: ");
+    display.print(volume);
+    
+    display.setCursor(0, 10);
+    display.print("Bal: ");
+    display.print(balance);
+    
+    display.setCursor(0, 20);
+    display.print(muteState ? "Muted" : "Unmuted");
+    
+    display.display();
 }
